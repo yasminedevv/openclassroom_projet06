08ceceba351bd77b1f0c69574c399f385960ca5a
yasmine <guerricheyasmine@gmail.com>
2024-08-18 19:39:49 +0200

    création routes et authentification

---
+++ Backend/models/book.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+
+const bookSchema = mongoose.Schema({
+
+  userId: { type: String, required: true },
+
+  title: { type: String, required: true },
+
+  author: { type: String, required: true },
+
+  imageUrl: { type: String, required: true },
+
+  year: { type: Number, required: true },
+
+  genre: { type: String, required: true },
+
+  ratings: [
+
+    {
+
+        userId: { type: String },
+
+        grade: { type: Number },
+
+    }
+
+  ],
+
+  averageRating: { type: Number , default:0 },
+
+});
+
+
+module.exports = mongoose.model('Book', bookSchema);
--- Backend/routes/books.js
+++ Backend/routes/books.js
@@ -5,9 +5,9 @@ const auth = require('../middleware/auth');
 
 const booksCtrl = require('../controllers/books')
 
-router.get('/', auth, booksCtrl.getAllBooks);
+router.get('/',  booksCtrl.getAllBooks);
 router.post('/', auth, booksCtrl.createBook);
-router.get('/:id', auth, booksCtrl.getOneBook);
+router.get('/:id', booksCtrl.getOneBook);
 router.put('/:id', auth, booksCtrl.modifyBook);
 router.delete('/:id', auth, booksCtrl.deleteBook);
 
