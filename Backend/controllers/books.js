7a8bd6620ab761bac570acc75d705b2589181313
yasmine <guerricheyasmine@gmail.com>
2024-08-25 00:55:16 +0200

    calcul average rating

--- Backend/controllers/books.js
+++ Backend/controllers/books.js
@@ -1,13 +1,8 @@
 const Book = require('../models/book');
+const average = require('../utils/average');
 const fs = require('fs');
 
-function average(array) {
-    let sum = 0;
-    for (let nb of array) {
-        sum += nb;
-    };
-    return (sum/array.length).toFixed(1);
-};
+
 
 exports.createBook = (req, res, next) => {
     const bookObject = JSON.parse(req.body.book);
@@ -54,13 +49,13 @@ exports.deleteBook = (req, res, next) => {
     Book.findOne({ _id: req.params.id})
         .then(book => {
             if (book.userId != req.auth.userId) {
-                res.status(401).json({message: 'Not authorized'});
+                res.status(403).json({ message: '403: unauthorized request' });
             } else {
                 const filename = book.imageUrl.split('/images/')[1];
                 fs.unlink(`images/${filename}`, () => {
                     Book.deleteOne({_id: req.params.id})
                         .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
-                        .catch(error => res.status(401).json({ error }));
+                        .catch(error => res.status(404).json({ error }));
                 });
             }
         })
@@ -78,51 +73,97 @@ exports.getAllBooks = (req, res, next) => {
 exports.getBestRating = (req, res, next) => {
     Book.find().limit(3)
     .then((books)=>res.status(200).json(books))
-    .catch((error)=>res.status(400).json({error}));
+    .catch((error)=>res.status(404).json({error}));}
 
 
 
-    /* Book.find()
-        .then(books =>
-            console.log(books)
-           books.sort(function (a, b) {
-                return b.averageRating - a.averageRating;
+
+exports.createRating = (req, res, next) => 
+    
+exports.createRating = (req, res, next) => {
+
+
+
+    if (0 <= req.body.rating <= 5) {
+
+    
+
+        const ratingObject = { ...req.body, grade: req.body.rating };
+
+       
+
+        delete ratingObject._id;
+
+
+        Book.findOne({_id: req.params.id})
+
+            .then(book => {
+
+                const newRatings = book.ratings;
+
+                const userIdArray = newRatings.map(rating => rating.userId);
+
+        
+                if (userIdArray.includes(req.auth.userId)) {
+
+                    res.status(403).json({ message : 'Not authorized' });
+
+                } else {
+
+                   
+              newRatings.push(ratingObject);
+
+                    const grades = newRatings.map(rating => rating.grade);
+
+                    const averageGrades = average.average(grades);
+
+                    book.averageRating = averageGrades;
+
+                
+                    Book.updateOne({ _id: req.params.id }, { ratings: newRatings, averageRating: averageGrades, _id: req.params.id })
+
+                        .then(() => { res.status(201).json()})
+
+                        .catch(error => { res.status(400).json( { error })});
+
+                    res.status(200).json(book);
+
+                }
+
+            })
+
+            .catch((error) => {
+
+                res.status(404).json({ error });
+
             });
-            const booksBestRating = [books[0], books[1], books[2]];
-            for (let book of booksBestRating) {
-                book._id = book._id.toString();
-            }
-            console.log(booksBestRating);
-            res.status(200).json(booksBestRating);
-        )
-        .catch(error => res.status(400).json({ error }));*/
+
+    } else {
+
+        res.status(400).json({ message: 'La note doit être comprise entre 1 et 5' });
+
+    }
+
 };
 
-exports.createRating = (req, res, next) => {
-    if (0<= req.body.rating <=5) {
-        const ratingObject = { ...req.body, grade: req.body.rating };
-        delete ratingObject._id;
-        Book.findOne({_id: req.params.id})
-            .then(book => {
-                const newRatings = book.ratings;
-                const userIdArray = newRatings.map(rating => rating.userId);
-                if (userIdArray.includes(req.auth.userId)) {
-                    res.status(401).json({ message : 'Not authorized' });
-                } else {
-                    newRatings.push(ratingObject);
-                    const grades = newRatings.map(rating => rating.grade);
-                    const averageGrades = average(grades);
-                    book.averageRating = averageGrades;
-                    Book.updateOne({ _id: req.params.id }, { ratings: newRatings, averageRating: averageGrades, _id: req.params.id })
-                        .then(() => { res.status(201).json()})
-                        .catch(error => { res.status(400).json( { error })});
-                    res.status(200).json(book);
-                }
-            })
-            .catch((error) => {
-                res.status(400).json({ error });
-            });
-    } else {
-        res.status(400).json({ message: 'La note doit être comprise entre 1 et 5' });
-    }
+
+
+
+
+
+
+// GET => Récupération des 3 livres les mieux notés
+
+exports.getBestRating = (req, res, next) => {
+
+    // Récupération de tous les livres
+
+    // Puis tri par rapport aux moyennes dans l'ordre décroissant, limitation du tableau aux 3 premiers éléments
+
+    Book.find().sort({averageRating: -1}).limit(3)
+
+        .then((books)=>res.status(200).json(books))
+
+        .catch((error)=>res.status(404).json({ error }));
+
 };
