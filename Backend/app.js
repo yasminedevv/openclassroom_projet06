4bb75778627e54005daaabe2600c0ee6ea00f9ff
yasmine <guerricheyasmine@gmail.com>
2024-08-25 00:06:06 +0200

    ajout des livres et notations

--- Backend/app.js
+++ Backend/app.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
 });
 
 //Middlleware//
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/auth', userRoutes);
 app.use('/api/books', booksRoutes);
 
--- Backend/routes/books.js
+++ Backend/routes/books.js
@@ -3,12 +3,15 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 
+const multer = require('../middleware/multer-config');
 const booksCtrl = require('../controllers/books')
 
-router.get('/',  booksCtrl.getAllBooks);
-router.post('/', auth, booksCtrl.createBook);
+router.get('/', booksCtrl.getAllBooks);
+router.post('/', auth, multer, booksCtrl.createBook);
 router.get('/:id', booksCtrl.getOneBook);
-router.put('/:id', auth, booksCtrl.modifyBook);
+router.put('/:id', auth, multer, booksCtrl.modifyBook);
 router.delete('/:id', auth, booksCtrl.deleteBook);
+router.get('/bestrating', booksCtrl.getBestRating);
+router.post('/:id/rating', auth, booksCtrl.createRating)
 
 module.exports = router;
